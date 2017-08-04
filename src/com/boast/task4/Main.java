9ade1dd380d2e2490991b934816c3c7e61eb47de
Oleh Bondarenko <boa.st@ya.ru>
2017-08-04 19:14:39 +0300

    Initial commit

---
+++ src/com/boast/task4/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Main.java 04/08/2017
+ *
+ * Created by Bondarenko Oleh
+ */
+
+
+package com.boast.task4;
+
+import java.lang.reflect.Field;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+	    String str = "Initialized string";
+
+	    Class<?> clazz = str.getClass();
+
+        Field field = clazz.getDeclaredField("value");
+        field.setAccessible(true);
+
+        System.out.println(str);
+
+        field.set(str, "Modified string".toCharArray());
+        System.out.println(str);
+    }
+}
